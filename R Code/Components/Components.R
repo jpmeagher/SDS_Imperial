b50782b09463d9823dfeb4be2e8cef90fbfe6d41
Joe Meagher <J.Meagher@warwick.ac.uk>
2017-05-08 09:35:37 +0100

    Datasets

--- R Code/Components/Components.R
+++ R Code/Components/Components.R
@@ -3,7 +3,7 @@ set.seed(8)
 
 # Create the samples of calls balanced by species and individual.
 N <- 1000
-resampled_calls <- balanced_samples(rescale_smooth_densities(), n_samples = N)
+resampled_calls <- balanced_samples(smooth_and_regularise_call_densities(), n_samples = N)
 
 # Proportion of variance accounted for by each component.
 get_proportion_variance <- function(df_col){
@@ -20,8 +20,9 @@ mean_proportion_variance <- apply(sample_proportion_variance, 1, mean)
 plot(mean_proportion_variance)
 mean_proportion_variance[1:10]
 plot(cumsum(mean_proportion_variance))
+cumsum(mean_proportion_variance)[1:10]
 
-sum(sample_proportion_variance[9,] > 0.03)
+sum(sample_proportion_variance[7,] > 0.03)
 proportion_mask <- (sample_proportion_variance > 0.03)
 sum(proportion_mask)
 # The centering / mean curves for the samples.
@@ -33,9 +34,9 @@ get_centring_curve <- function(df_col){
 }
 
 sample_centring_curve <- apply(resampled_calls, 2, get_centring_curve)
-matplot(sample_centring_curve[, 1:100], type = 'l')
+matplot(sample_centring_curve, type = 'l')
 mean_centring_curve <- apply(sample_centring_curve, 1, mean)
-plot(mean_centring_curve, type = 'l')
+lines(mean_centring_curve, lwd = 3)
 
 # Get 10 most descriptive components.
 get_components <-function(df_col, n_components = 10){
@@ -45,67 +46,97 @@ get_components <-function(df_col, n_components = 10){
   return(pca$rotation[, 1:n_components])
 }
 
-examined_components <- 10
-sample_components <- apply(resampled_calls, 2, get_components, n_components = examined components)
-sample_components <- array(sample_components, dim = c(208, 10, N))
-matplot(sample_components[,6,], type = 'l')
-proportion_mask <- proportion_mask[1:10, ]
+examined_components <- 7
+sample_components <- apply(resampled_calls, 2, get_components, n_components = examined_components)
+sample_components <- array(sample_components, dim = c(208, examined_components, N))
+
+proportion_mask <- proportion_mask[1:examined_components, ]
 sum(proportion_mask)
 
 # Identify component peak frequencies
 peak_frequency <- apply(abs(sample_components), c(2,3), which.max)
 boxplot_components <- boxplot(t(peak_frequency), xlab = 'Principal Components', ylab = 'Frequency', main = 'Boxplot of Max Asolute value Position')
 boxplot_components$stats
-matplot(abs(sample_components[,6,]), type = 'l')
 
 
-# Select components based on peak frequency
-# First Component
+
+
+# Orientate components so absolute maximum is positive
 absmax <- function(x) { x[which.max( abs(x) )]}
 
-sample_first_component <- apply(sample_components[, 1, ], 2, function(x) sign(absmax(x))*x)
+# first component
+matplot(sample_components[,1,], type = 'l')
+first_sample <-  apply(sample_components[, 1, ], 2, function(x) sign(absmax(x))*x)
+matplot(first_sample, type = 'l')
+
+first_mean <- apply(first_sample, 1, mean)
+lines(first_mean, lwd = 3)
+
+# second component
+matplot(sample_components[,2,], type = 'l')
+second_sample <- apply(sample_components[, 2, ], 2, function(x) sign(absmax(x[50:100]))*x)
+
+matplot(second_sample, type = 'l')
+
+second_mean <- apply(second_sample, 1, mean)
+lines(second_mean, lwd = 3)
+
+# subsequent components
+identify <- function(components = sample_components[, -c(1,2),], max_range = c(41, 45)){
+  peaks <- apply(abs(components), c(2,3), which.max)
+
+  mask <- peaks >= max_range[1] & peaks <= max_range[2]
+
+  mask <- array(rep(mask, dim(components)[1]), dim = c(dim(components)[2:3], dim(components)[1]))
+  mask <- aperm(mask, c(3,1,2))
+
+  curves <- apply(components, c(2,3), function(x) sign(absmax(x))*x)
+  curves <- curves[mask]
+  curves <- matrix(curves, ncol = dim(components)[1], byrow = T)
+
+  return(t(curves))
+}
+
+# third
+third_sample <- identify(max_range = c(41,45))
+matplot((third_sample), type = 'l')
+
+third_mean <- apply(third_sample, 1, mean)
+lines(third_mean, lwd = 3)
+
+# fourth
+fourth_sample <- identify(max_range = c(30,34))
+matplot((fourth_sample), type = 'l')
+
+fourth_mean <- apply(fourth_sample, 1, mean)
+lines(fourth_mean, lwd = 3)
+
+# fifth
+fifth_sample <- identify(max_range = c(15,19))
+matplot((fifth_sample), type = 'l')
+
+fifth_mean <- apply(fifth_sample, 1, mean)
+lines(fifth_mean, lwd = 3)
+
+# sixth
+sixth_sample <- identify(max_range = c(24, 28))
+matplot((sixth_sample), type = 'l')
+
+sixth_mean <- apply(sixth_sample, 1, mean)
+lines(sixth_mean, lwd = 3)
+
+components <- cbind(first_mean, second_mean, third_mean, fourth_mean, fifth_mean, sixth_mean)
+
+principal_components <- apply(components, 1, '/', sqrt(diag(t(components)%*%(components))))
+image(t(principal_components)%*%principal_components)
+image(((principal_components)%*%t(principal_components)))
+matplot(t(principal_components), type = 'l', lty = 1)
+
+getwd()
+
+spectral_fpca <- t(principal_components)
+spectral_mean <- mean_centring_curve
 
 
-matplot(sample_first_component, type = 'l')
-lines(mean_first_component, lwd = 3)
-
-
-# Second Component
-matplot(sample_components[,3,], type = 'l')
-
-sample_second_component <- apply(sample_components[, 2, ], 2, function(x) sign(absmax(x[50:100]))*x)
-
-
-matplot(sample_second_component, type = 'l')
-lines(mean_second_component, lwd = 3)
-
-which.min(sample_components[, 2, 1])
-
-# Identify curves where components have switched order and switch them back.
-mask <- apply(sample_first_component, 2, function(x) absmax(x[150:200]) < 0)
-matplot(sample_second_component[, mask], type = 'l')
-
-final_first_component <- cbind(sample_first_component[, !mask], sample_second_component[, mask] )
-final_second_component <- cbind(sample_second_component[, !mask], sample_first_component[, mask] )
-
-mean_second_component <- apply(final_second_component, 1, mean)
-mean_first_component <- apply(final_first_component, 1, mean)
-)
-
-# Identify next 5 components
-# Peaks under 50
-res_proportion_mask <- proportion_mask[3:10,]
-res_proportion_mask <- array(rep(res_proportion_mask, 208), c(8, 1000, 208))
-identical(res_proportion_mask[1,,], res_proportion_mask[2,,])
-res_proportion_mask <- aperm(res_proportion_mask, c(3,1,2))
-res_components <- sample_components[, 3:10,]
-res_components <- res_components[res_proportion_mask]
-plot(res_components[4*208+(1:208)])
-dim(res_components)
-dim(res_proportion_mask)
-# Peak frequency at 42 to 45
-peak_mask <- (peak_frequency == 42 | peak_frequency == 43 | peak_frequency == 44)
-sum(peak_mask)
-matplot(sample_components[, ])
-
-
+devtools::use_data(spectral_fpca)
+devtools::use_data(spectral_mean)
